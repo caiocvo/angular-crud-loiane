11a7e763f480bf3a3b23aad2e76a568ed91ae411
caiocvo <seza45670@gmail.com>
2026-07-01 23:11:44 -0300

    testando visibilidade dos cursos

--- crud-angular/src/app/courses/courses/courses.ts
+++ crud-angular/src/app/courses/courses/courses.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './courses.scss',
 })
 export class Courses {
-  courses: any[] = [];
+  courses: Course[] = [
+    { _id: 1, name: 'Angular', category: 'Front-end' },
+    { _id: 1, name: 'ReactJs', category: 'Front-end' },
+  ];
+  displayedColumns = ['name', 'category'];
 }
--- crud-angular/src/app/courses/model/course.ts
+++ crud-angular/src/app/courses/model/course.ts
@@ -1,1 +1,5 @@
-export interface Course {}
+export interface Course {
+  _id: number;
+  name: string;
+  category: string;
+}
