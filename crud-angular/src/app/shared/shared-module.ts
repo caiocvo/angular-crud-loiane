530d918e47037e497293c8f9a0db9a58841caca7
caiocvo <seza45670@gmail.com>
2026-09-20 16:38:26 -0300

    adicionando icones com pipe

--- crud-angular/src/app/shared/shared-module.ts
+++ crud-angular/src/app/shared/shared-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorDialog } from './components/error-dialog/error-dialog';
 import { AppMaterialModule } from './app-material/app-material-module';
+import { CategoryPipe } from './pipes/category-pipe';
 @NgModule({
   declarations: [ErrorDialog], //Só componentes
-  imports: [CommonModule, AppMaterialModule], //Módulos no import
-  exports: [ErrorDialog],
+  imports: [CommonModule, AppMaterialModule, CategoryPipe], //Módulos no import
+  exports: [ErrorDialog, CategoryPipe],
 })
 export class SharedModule {}
